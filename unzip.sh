56feed55fe2af5515fd66c6c151ec5517f923085
Egor <ykotko@mirantis.com>
2013-11-27 16:59:10 +0200

    Changes with container path

--- unzip.sh
+++ unzip.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [[ -z "$1" ]]; then
      echo "NO <ENV_NAME>"
 #    echo "USAGE: $0 ENV_NAME"
@@ -29,8 +28,6 @@ for i in ${nets_start[@]} ; do
 virsh net-start ${i}
 virsh net-autostart ${i}
 done
-rm -rf $net_path # Cleaning dir "~/network_xml_save"
-
 
 echo 'Define VM'
 nodes=(`ls -l $node_path | awk '{print $9}'`)
@@ -40,32 +37,23 @@ for i in ${nodes[@]} ; do
 done
 node_start=(`virsh list --all | awk '{print $2}' | sed  -n "/^$env_name/p"`)
 
-#for i in ${node_start[@]} ; do
-#virsh start ${i}
-#virsh autostart ${i}
-#done
-rm -rf $node_path   # Cleaning dir "~/node_xml_save"
-
-
 echo 'Define snapshots'
-#snapshot_list=(`ls -l $snapshot_path | awk '{print $9}' | tail -f -n +2`)
 snapshot_list=(`ls -1 $snapshot_path`)
 for i in ${node_start[@]} ; do
-    #snapshots=(`virsh snapshot-list ${i} | tail -f | awk '{print $1}'| sed  -n "/^$env_name/p"`)
     virsh snapshot-create ${i} $snapshot_path/$i*xml # $snapshot_list
-    #virsh snapshot-dumpxml ${i} $snapshots > $snapshot_path/$snapshots.xml
 done
 
-
-
-
-rm -rf $snapshot_path #Cleaning dir "~/snapshot_xml_save"
-
-
-##############################################################################################
 echo 'Snapshot reverting'
 for i in ${node_start[@]};do
-snapshot_list=(`virsh snapshot-list ${i} | tail -f -n 2 |awk '{print $1}' | sed  -n "/^$env_name/p"`)
-virsh snapshot-revert ${i} $snapshot_list
+snapshot_list_name=(`virsh snapshot-list ${i} | tail -f -n +3 |awk '{print $1}' | sed  -n "/^$env_name/p"`)
+virsh snapshot-revert ${i} $snapshot_list_name
+echo "Ready ${i}"
 done
 
+
+
+#Clening section
+rm -rf $net_path # Cleaning dir "~/network_xml_save"
+rm -rf $node_path   # Cleaning dir "~/node_xml_save"
+rm -rf $snapshot_path #Cleaning dir "~/snapshot_xml_save"
+echo "-=Done=-"
--- zip.sh
+++ zip.sh
@@ -10,19 +10,16 @@ env_name="$1"
 
 rm -f $env_name".tar.*"
 
-
 #Path where configuration xmls of VMs whill be saves
 node_path=~/node_xml_save
 #Path where configuration xmls of networks will be saved
 net_path=~/network_xml_save/
 #Path where configuration xmls of snapshots will be saved
 snapshot_path=~/snapshot_xml_save/
-
 GZ=`which pigz || which gzip`
 mkdir -p ~/network_xml_save
 mkdir -p ~/node_xml_save
 mkdir -p ~/snapshot_xml_save
-
 
 echo 'VM snapshot creation'
 nodes=(`virsh list --all | awk '{print $2}' | sed  -n "/^$env_name/p"`) 
@@ -30,15 +27,11 @@ echo ${#nodes[*]}
 for i in ${nodes[@]} ; do
     virsh snapshot-create-as ${i} ${i}_$(date '+%Y-%m-%d-%H-%M')
     echo "${i} is  Ready"
-done
-
-echo 'Nodes .xmls save'
-for i in ${nodes[@]} ; do
     virsh dumpxml ${i} > $node_path/node_snapshot_${i}.xml
     echo "${i} is  Ready"
-done 
-
-vm_path_one=$(egrep "source file" $node_path/node_snapshot_$nodes.xml | cut -d "'" -f 2 | xargs dirname) 
+    snapshots=(`virsh snapshot-list ${i} | tail -f | awk '{print $1}'| sed  -n "/^$env_name/p"`)
+    virsh snapshot-dumpxml ${i} $snapshots > $snapshot_path/$snapshots.xml    
+done
 
 echo 'Network .xmls save' 
 nets=(`virsh net-list --all | awk '{print $1}' | sed  -n "/$env_name/p"`)
@@ -48,20 +41,19 @@ for i in ${nets[@]} ; do
     echo "${i} is Ready"
 done
 
-echo 'Snapshots .xmls save'
-for i in ${nodes[@]} ; do
-    snapshots=(`virsh snapshot-list ${i} | tail -f | awk '{print $1}'| sed  -n "/^$env_name/p"`)
-    virsh snapshot-dumpxml ${i} $snapshots > $snapshot_path/$snapshots.xml
-done
-
+list=($(ls -1 ~/node_xml_save | tail -f -n 1))
 echo 'Creation of archive'
+vm_path_one=$(egrep "source file" $node_path/$list | cut -d "'" -f 2 | xargs dirname)
 >>$env_name.tar
 containers=(`ls -l $vm_path_one | awk '{print $9}' | sed  -n "/^$env_name/p"`)
 echo 'Images number:'${#containers[*]}
 for i in ${containers[@]} ; do
-    vm_path_two=$(egrep "source file" $node_path/node_snapshot_$nodes.xml | cut -d "'" -f 2 | xargs dirname)
-    tar --append -f $env_name.tar $vm_path_two/${i} 
+    tar --append -f $env_name.tar $vm_path_one/${i} 
 done
 tar --append -f $env_name.tar $net_path $node_path $snapshot_path
 $GZ -1 $env_name.tar
+#Clening section
+rm -rf $net_path # Cleaning dir "~/network_xml_save"
+rm -rf $node_path   # Cleaning dir "~/node_xml_save"
+rm -rf $snapshot_path #Cleaning dir "~/snapshot_xml_save"
 echo "-=DONE=-"
